77d667daa14120e7d1ab8c660e18eab7e7334254
ram kumar <rr697144@gmail.com>
2025-08-25 22:59:15 +0530

    properties injection in layerd app

---
+++ Proj11_Realtime_LayeredApp_PropertiesIj/src/main/java/com/app/student/test/LayerAppTest.java
@@ -0,0 +1,39 @@
+package com.app.student.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.app.student.controller.StudentMainController;
+import com.app.student.vo.StudentVo;
+
+public class LayerAppTest {
+	public static void main(String[] args) {
+		System.out.println("Layered App");
+		
+		ApplicationContext context = null;
+		context = new ClassPathXmlApplicationContext("config.xml");
+		
+		StudentMainController controller = null;
+		
+		controller = context.getBean("controller",StudentMainController.class);
+		
+		StudentVo vo = new StudentVo();
+		
+		vo.setId("1005");
+		vo.setName("money kumar");
+		vo.setEmail("money@gmail.com");
+		vo.setCourseName("java backend");
+		vo.setObtMarks("300");
+		
+		try {
+			controller.registerStudentRequest(vo);
+			
+		} catch (Exception e) {
+			System.out.println("something went wrong");
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
