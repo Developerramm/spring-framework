104c698259a78f684112846180892fae65da827d
ram kumar <rr697144@gmail.com>
2025-08-26 07:41:13 +0530

    bean inheritance part 1

---
+++ Proj12_BeanInheritance/src/main/java/com/app/test/BeanInheritanceTest.java
@@ -0,0 +1,28 @@
+package com.app.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.app.beans.Car;
+
+public class BeanInheritanceTest {
+	
+	public static void main(String[] args) {
+		ApplicationContext context = null;
+		
+		context = new ClassPathXmlApplicationContext("config.xml");
+		
+		Car car = null;
+		
+		car = context.getBean("car",Car.class);
+		
+		System.out.println(car);
+		
+		System.out.println("_______________________________________");
+		
+		car = context.getBean("car1",Car.class);
+		
+		System.out.println(car);
+	}
+
+}
