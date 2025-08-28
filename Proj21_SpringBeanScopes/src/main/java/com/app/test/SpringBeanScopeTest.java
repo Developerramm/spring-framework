d758abe1b9832e8b1cdb42ebfcf34f9221d9cf65
ram kumar <rr697144@gmail.com>
2025-08-28 14:14:37 +0530

    prototype scope bean in spring core

--- Proj21_SpringBeanScopes/src/main/java/com/app/test/SpringBeanScopeTest.java
+++ Proj21_SpringBeanScopes/src/main/java/com/app/test/SpringBeanScopeTest.java
@@ -1,8 +1,10 @@
 package com.app.test;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.app.beans.Faculty;
 import com.app.beans.Student;
 
 public class SpringBeanScopeTest {
@@ -20,6 +22,27 @@ public class SpringBeanScopeTest {
 		
 		Student s1 = context.getBean("student",Student.class);
 		System.out.println(s1.hashCode());
+		
+		
+		student = context.getBean("student2",Student.class);
+		
+		System.out.println(student.hashCode());
+		
+		System.out.println("--------------------------------------------");
+		
+		
+		Faculty faculty = context.getBean("faculty",Faculty.class);
+		System.out.println(faculty.hashCode());
+		
+		Faculty f1 = context.getBean("faculty",Faculty.class);
+		System.out.println(f1.hashCode());
+		
+		Faculty f2 = context.getBean("faculty",Faculty.class);
+		System.out.println(f2.hashCode());
+		
+		
+		
+		((AbstractApplicationContext)context).close();
 		
 		
 	}
