df2a8a7d4fa5e8d177fb29d9859c64a2e2f31822
ram kumar <rr697144@gmail.com>
2025-08-29 08:56:01 +0530

    properties file in realtime layered app

---
+++ Proj24_LayeredApplication_Properties_Files/src/main/java/com/app/student/test/LayerAppTest.java
@@ -0,0 +1,40 @@
+package com.app.student.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.app.student.controller.StudentMainController;
+import com.app.student.vo.StudentVo;
+
+public class LayerAppTest {
+	public static void main(String[] args) {
+		System.out.println("Layered App");
+		
+		ApplicationContext context = null;
+		context = new ClassPathXmlApplicationContext("config.xml");
+		
+		StudentMainController controller = null;
+		
+		controller = context.getBean("controller",StudentMainController.class);
+		
+		StudentVo vo = new StudentVo();
+		
+		vo.setId("1012");
+		vo.setName("Sarita verma");
+		vo.setEmail("sarita@gmail.com");
+		vo.setCourseName("Angular developer");
+		vo.setObtMarks("350");
+		
+		try {
+			controller.registerStudentRequest(vo);
+			
+		} catch (Exception e) {
+			System.out.println("something went wrong");
+			e.printStackTrace();
+		}
+		
+		((AbstractApplicationContext)context).close();
+	}
+
+}
