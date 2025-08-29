1e7e165c4ad1a52ba95bf0593dafc210b2564fd6
ram kumar <rr697144@gmail.com>
2025-08-29 11:10:51 +0530

    internationalization core java

---
+++ Proj25_Internationalization_CoreJava/src/com/app/test/InternationalisationTest.java
@@ -0,0 +1,25 @@
+package com.app.test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class InternationalisationTest {
+	
+	public static void main(String[] args) {
+		
+		// internationalisation
+		
+		Locale locale = null;
+		locale = new Locale(args[0],args[1]);
+		
+		ResourceBundle bundle = ResourceBundle.getBundle("com/app/test/App",locale);
+		
+		System.out.println(bundle.getString("loginBtn")
+				+ " " + bundle.getString("regBtn") + " "
+				+ bundle.getString("buyBtn")
+				
+				);
+		
+	}
+
+}
