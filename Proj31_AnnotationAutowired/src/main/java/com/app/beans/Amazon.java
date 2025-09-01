2a79e8df109f7e6848707a6eabbaaba38d3c5733
ram kumar <rr697144@gmail.com>
2025-09-01 12:58:14 +0530

    @Qualifier annotation

--- Proj31_AnnotationAutowired/src/main/java/com/app/beans/Amazon.java
+++ Proj31_AnnotationAutowired/src/main/java/com/app/beans/Amazon.java
@@ -1,10 +1,12 @@
 package com.app.beans;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Amazon {
 	
 	@Autowired
+	@Qualifier("bluedart")
 	private LogisticsService logisticsService;
 	
 	public void trackOrder(int orderId) {
