c6a0cde0144144f48dd0a707c7485eb5a94ff419
ram kumar <rr697144@gmail.com>
2025-09-25 19:18:57 +0530

    spring security first project

---
+++ SpringSecurity_Proj1_Basic/src/main/java/com/app/security/SpringSecurityProj1BasicApplication.java
@@ -0,0 +1,14 @@
+package com.app.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProj1BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityProj1BasicApplication.class, args);
+		System.out.println("Spring project 1");
+	}
+
+}
